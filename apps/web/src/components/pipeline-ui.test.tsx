f0ffd8ce92a544f5573ac95e116fdb19aae6862c
Mo Qiwen <330115138@qq.com>
2026-09-12 16:54:17 +0800

    test: fix pipeline UI test runtime

--- apps/web/src/components/pipeline-ui.test.tsx
+++ apps/web/src/components/pipeline-ui.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 import { describe, expect, test } from "vitest";
 import { CompilerDemo } from "./compiler-demo";
